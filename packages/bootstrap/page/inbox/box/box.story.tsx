a1f681f418f288effd15e7016c4528dae75a25ad
Newton <5769156+iamnewton@users.noreply.github.com>
2024-11-11 14:49:47 -0800

    chore: fix build types
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- packages/bootstrap/page/inbox/box/box.story.tsx
+++ packages/bootstrap/page/inbox/box/box.story.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/nextjs";
+import type { Meta, StoryObj } from "@storybook/react";
 import { expect, userEvent, findByRole, within } from "@storybook/test";
 import { http, HttpResponse } from "msw";
 
