a4c7756ddc50930a9541e1e1a5875f0b8213bc92
Newton <5769156+iamnewton@users.noreply.github.com>
2024-11-12 20:39:08 -0800

    chore: remove coupling of mantine
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- packages/bootstrap/src/bootstrap/bootstrap.tsx
+++ packages/bootstrap/src/bootstrap/bootstrap.tsx
@@ -1,4 +1,3 @@
-import { MantineProvider } from "@mantine/core";
 import Configuration, { type IConfiguration } from "../configuration/";
 import Environment from "../environment/";
 import Konami from "../konami/";
@@ -6,7 +5,6 @@ import Loading from "../loading/";
 import { type LoadingProps } from "../loading/loading";
 import Location from "../location/";
 import Storage from "../storage/";
-import { theme } from "../theme";
 import type { WithChildren } from "../type";
 import { Container } from "./container";
 
@@ -21,17 +19,15 @@ export function Bootstrap(props: BootstrapProps) {
 		<Configuration.Provider conf={props.conf}>
 			<Environment.Provider>
 				<Storage.Provider conf={props.conf?.application}>
-					<MantineProvider theme={theme}>
-						<Loading.Provider loader={props.loader}>
-							<Location.Provider>
-								<Container conf={props.conf}>
-									<Konami.Provider>
-										{props.children}
-									</Konami.Provider>
-								</Container>
-							</Location.Provider>
-						</Loading.Provider>
-					</MantineProvider>
+					<Loading.Provider loader={props.loader}>
+						<Location.Provider>
+							<Container conf={props.conf}>
+								<Konami.Provider>
+									{props.children}
+								</Konami.Provider>
+							</Container>
+						</Location.Provider>
+					</Loading.Provider>
 				</Storage.Provider>
 			</Environment.Provider>
 		</Configuration.Provider>
--- packages/bootstrap/src/theme.ts
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { createTheme, type MantineTheme } from "@mantine/core";
-
-export const theme: MantineTheme = createTheme({
-	/* mantine theme overrides */
-});
