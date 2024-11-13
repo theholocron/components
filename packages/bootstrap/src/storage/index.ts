0811497fcf8cdfff9cbe38e53960c4bde95acec4
Newton <5769156+iamnewton@users.noreply.github.com>
2024-11-12 20:12:46 -0800

    chore: fix types
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- packages/bootstrap/src/storage/index.ts
+++ packages/bootstrap/src/storage/index.ts
@@ -2,6 +2,6 @@ import { type TSessionStorage } from "@theholocron/utils-storage";
 import { Provider } from "./storage";
 
 export { useStorage } from "./storage";
-export type { TSessionStorage } from "./storage";
 
+export type { TSessionStorage };
 export default { Provider };
